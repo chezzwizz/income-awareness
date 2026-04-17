0e65de724f7daff94e8787275b94ad1b2d48fbb2
One of us! One of us! <chezzwizz@users.noreply.github.com>
2026-04-17 12:32:08 -0700

    Create service_worker.js

---
+++ chrome/service_worker.js
@@ -0,0 +1,3 @@
+// Placeholder service worker (MV3)
+self.addEventListener('install', () => console.log('Income Awareness Service Worker (SW) installed'));
+self.addEventListener('activate', () => console.log('Income Awareness SW activated'));
